5a067ba67b703eae697c0d36558bfe2752438b68
caseyjames95 <caseypurves@gmail.com>
2019-09-30 19:12:46 -0700

    Basic page setup
    Setup page with inputs.
    
    Started javascript.

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+const addTrain = function (e) {
+    e.preventDefault()
+    return tName
+}
+
+document.getElementById('tName').value
+document.getElementById('tDest').value
+document.getElementById('tFirst').value
+document.getElementById('tFreq').value
+
+document.getElementById('tSubmit').addEventListener('click'.addTrain(e) = {
+})
+
