37c640b62bc90b81bec4d9fbeae2e0d55746f7dc
caseyjames95 <caseypurves@gmail.com>
2019-09-30 23:34:16 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,41 @@
+let trains = {}
+let newTrain = {}
 let name = ''
 let dest = ''
 let first = 0
 let freq = 0
+e = ''
 
-const addTrain = function () {
-    e.preventDefault()
+// Get stored information from Firebase
+
+
+// Create train info row
+const createRow = function () {
+    newTrain = document.createElement('td')
+    document.getElementById('newTrain').innerHTML(`
+        <td>${name}</td>
+        <td>${dest}</td>
+        <td>${first}</td>
+        <td>${freq}</td>
+        `)
+    document.getElementById('trainRow').append('tr')
 }
 
-document.getElementById('tName').value
-document.getElementById('tDest').value
-document.getElementById('tFirst').value
-document.getElementById('tFreq').value
+// Give variables a value
+const addTrain = function (e) {
+    e.preventDefault()
+    name = document.getElementById('tName').value
+    dest = document.getElementById('tDest').value
+    first = document.getElementById('tFirst').value
+    freq = document.getElementById('tFreq').value
+    createRow()
+}
 
-document.getElementById('tSubmit').addEventListener('click'.addTrain() = {
-    
-})
+
+
+
+
+
+// Click event to start
+document.getElementById('tSubmit').addEventListener('click', addTrain)
 
